5e9348ab648c954d487fd466006f8de3c6bd6783
Ye Joo Park <subwaymatch@gmail.com>
2019-05-29 15:27:43 -0700

    Setup, form validation using hooks

---
+++ src/components/Auth/Login.js
@@ -0,0 +1,81 @@
+import React from "react";
+
+import useFormValidation from "./useFormValidation";
+import validateLogin from "./validateLogin";
+
+const INITIAL_STATE = {
+  name: "",
+  email: "",
+  password: ""
+};
+
+function Login(props) {
+  const {
+    handleChange,
+    handleBlur,
+    handleSubmit,
+    values,
+    errors,
+    isSubmitting
+  } = useFormValidation(INITIAL_STATE, validateLogin);
+
+  const [login, setLogin] = React.useState(true);
+  return (
+    <div>
+      <h2 className="mv3">{login ? "Login" : "Create Account"}</h2>
+      <form onSubmit={handleSubmit} className="flex flex-column">
+        {!login && (
+          <input
+            onChange={handleChange}
+            values={values.name}
+            name="name"
+            type="text"
+            placeholder="Your name"
+            autoComplete="off"
+          />
+        )}
+        <input
+          onChange={handleChange}
+          onBlur={handleBlur}
+          classNames={errors.email && "error-input"}
+          values={values.email}
+          name="email"
+          type="email"
+          placeholder="Your email"
+          autoComplete="off"
+        />
+        {errors.email && <p className="error-text">{errors.email}</p>}
+        <input
+          onChange={handleChange}
+          onBlur={handleBlur}
+          classNames={errors.password && "error-input"}
+          values={values.password}
+          name="password"
+          type="password"
+          placeholder="Choose a secure password"
+        />
+        {errors.password && <p className="error-text">{errors.password}</p>}
+
+        <div className="flex mt3">
+          <button
+            type="submit"
+            className="button pointer mr2"
+            disabled={isSubmitting}
+            style={{ background: isSubmitting ? "grey" : "orange" }}
+          >
+            Submit
+          </button>
+          <button
+            type="button"
+            className="button pointer"
+            onClick={() => setLogin(prevLogin => !prevLogin)}
+          >
+            {login ? "need to create an account?" : "already have an account?"}
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
