d2ceb44711b95954e9eb3e32efb781fbdc3e519a
Ye Joo Park <subwaymatch@gmail.com>
2019-05-30 11:57:07 -0700

    Fetch and display links

--- src/components/Link/LinkList.js
+++ src/components/Link/LinkList.js
@@ -1,8 +1,39 @@
 import React from "react";
-import useAuth from "./../Auth/useAuth";
+import { FirebaseContext } from "../../firebase";
+import LinkItem from "./LinkItem";
 
 function LinkList(props) {
-  return <div>LinkList</div>;
+  const { firebase } = React.useContext(FirebaseContext);
+  const [links, setLinks] = React.useState([]);
+
+  // Run on component mount if empty array is passed
+  React.useEffect(() => {
+    getLinks();
+  }, []);
+
+  function getLinks() {
+    firebase.db.collection("links").onSnapshot(handleSnapshot);
+  }
+
+  function handleSnapshot(snapshot) {
+    const links = snapshot.docs.map(doc => {
+      return {
+        id: doc.id,
+        ...doc.data()
+      };
+    });
+
+    console.log({ links });
+    setLinks(links);
+  }
+
+  return (
+    <div>
+      {links.map((link, index) => (
+        <LinkItem key={link.id} showCount={true} link={link} index={index} />
+      ))}
+    </div>
+  );
 }
 
 export default LinkList;
