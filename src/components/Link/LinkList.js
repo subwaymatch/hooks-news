7f5259247f2410d017bc2046a7a4154a40d7779e
Ye Joo Park <subwaymatch@gmail.com>
2019-05-31 02:32:03 -0700

    Publish to Firebase hosting

--- src/components/Link/LinkList.js
+++ src/components/Link/LinkList.js
@@ -2,15 +2,17 @@ import React from "react";
 import { FirebaseContext } from "../../firebase";
 import LinkItem from "./LinkItem";
 import { LINKS_PER_PAGE } from "./../../utils/index";
-import axios from "axios"; 
+import axios from "axios";
 
 function LinkList(props) {
   const { firebase } = React.useContext(FirebaseContext);
   const [links, setLinks] = React.useState([]);
   const [cursor, setCursor] = React.useState(null);
+  const [loading, setLoading] = React.useState(false);
   const isNewPage = props.location.pathname.includes("new");
   const isTopPage = props.location.pathname.includes("top");
   const page = Number(props.match.params.page);
+  const linksRef = firebase.db.collection("links");
 
   // Run on component mount if empty array is passed
   React.useEffect(() => {
@@ -21,37 +23,39 @@ function LinkList(props) {
 
   function getLinks() {
     const hasCursor = Boolean(cursor);
+    setLoading(true);
 
     if (isTopPage) {
-      return firebase.db
-        .collection("links")
+      return linksRef
         .orderBy("voteCount", "desc")
         .limit(LINKS_PER_PAGE)
         .onSnapshot(handleSnapshot);
     } else if (page === 1) {
-      return firebase.db
-        .collection("links")
+      return linksRef
         .orderBy("created", "desc")
         .limit(LINKS_PER_PAGE)
         .onSnapshot(handleSnapshot);
     } else if (hasCursor) {
-      return firebase.db
-        .collection("links")
+      return linksRef
         .orderBy("created", "desc")
         .startAfter(cursor.created)
         .limit(LINKS_PER_PAGE)
         .onSnapshot(handleSnapshot);
     } else {
-      const offset = page * LINKS_PER_PAGE - LINKS_PER_PAGE; 
-      axios.get(`https://us-central1-hooks-news-743e1.cloudfunctions.net/linksPagination?offset=${offset}`)
+      const offset = page * LINKS_PER_PAGE - LINKS_PER_PAGE;
+      axios
+        .get(
+          `https://us-central1-hooks-news-743e1.cloudfunctions.net/linksPagination?offset=${offset}`
+        )
         .then(response => {
-          const links = response.data; 
-          const lastLink = links[links.length - 1]; 
-          setLinks(links); 
-          setCursor(lastLink); 
-        })
+          const links = response.data;
+          const lastLink = links[links.length - 1];
+          setLinks(links);
+          setCursor(lastLink);
+          setLoading(false);
+        });
 
-      return () => {}; 
+      return () => {};
     }
   }
 
@@ -66,6 +70,7 @@ function LinkList(props) {
     setLinks(links);
     const lastLink = links[links.length - 1];
     setCursor(lastLink);
+    setLoading(false);
   }
 
   function visitPreviousPage() {
@@ -83,7 +88,7 @@ function LinkList(props) {
   const pageIndex = page ? (page - 1) * LINKS_PER_PAGE + 1 : 1;
 
   return (
-    <div>
+    <div style={{ opacity: loading ? 0.25 : 1 }}>
       {links.map((link, index) => (
         <LinkItem
           key={link.id}
